214331af3fb65a0cbf2b5d22b7c1f47dc37e4f2b
Akvamarinika <rozespink@mail.ru>
2021-01-12 00:50:07 +0800

    Added ingredient check for each recipe

--- 2booleans/src/Main.java
+++ 2booleans/src/Main.java
@@ -3,18 +3,24 @@ public class Main {
     public static void main(String[] args) {
         int milkAmount = 200; // ml
         int powderAmount = 5; // g
-        int eggsCount = 3; // items
+        int eggsCount = 4; // items
         int sugarAmount = 5; // g
         int oilAmount = 30; // ml
         int appleCount = 8;
+        String str = "You can cook ";
 
         //powder - 400 g, sugar - 10 g, milk - 1 l, oil - 30 ml
-        System.out.println("Pancakes");
-
+        if (powderAmount >= 400 && sugarAmount >= 10 && milkAmount >= 1000 && oilAmount >= 30)
+            System.out.println(str + "Pancakes");
         //milk - 300 ml, powder - 5 g, eggs - 5
-        System.out.println("Omelette");
+        else if (milkAmount >= 300 && powderAmount >= 5 && eggsCount >= 5) {
+            System.out.println(str + "Omelette");
+         //apples - 3, milk - 100 ml, powder - 300 g, eggs - 4
+        } else if (appleCount >= 3 && milkAmount >= 100 && eggsCount >= 4) {
+            System.out.println(str + "Apple pie");
+        } else {
+            System.out.println("Insufficient amount of food for cooking!");
+        }
 
-        //apples - 3, milk - 100 ml, powder - 300 g, eggs - 4
-        System.out.println("Apple pie");
     }
 }
