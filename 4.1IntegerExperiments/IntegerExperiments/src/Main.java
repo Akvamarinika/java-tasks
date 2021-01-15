d3961c1ce1df177e2254d26af4c19057ee264a11
Akvamarinika <rozespink@mail.ru>
2021-01-15 21:45:17 +0800

    Fix bugs: replacing Integer with int in Container class. Added static method sumDigits.

--- 4.1IntegerExperiments/IntegerExperiments/src/Main.java
+++ 4.1IntegerExperiments/IntegerExperiments/src/Main.java
@@ -1,15 +1,27 @@
+/*
+    TODO:
+     1. Запустите код проекта IntegerExperiments. Найдите ошибку, исправьте её заменой Integer на int.
+     2. Реализуйте статический метод sumDigits, чтобы он считал сумму цифр в числе и возвращал сумму в результате своей работы.
+*/
+
 public class Main
 {
     public static void main(String[] args)
     {
         Container container = new Container();
         container.count += 7843;
+        System.out.println(sumDigits(container.count));
 
     }
 
-    public Integer sumDigits(Integer number)
+    public static Integer sumDigits(Integer number)
     {
-        //@TODO: write code here
-        return 0;
+        int sum = 0;
+        String str = number.toString();
+        for (int idx = 0; str.length() > idx; idx++){
+            sum += Integer.parseInt(String.valueOf(str.charAt(idx)));
+        }
+
+        return sum;
     }
 }
