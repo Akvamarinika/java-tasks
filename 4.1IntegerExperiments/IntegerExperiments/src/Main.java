85ac20d624936e8dec2bdb5bdb8fea270eef2d4f
Akvamarinika <rozespink@mail.ru>
2021-01-16 00:06:57 +0800

    Added static method sumDigitsChar.

--- 4.1IntegerExperiments/IntegerExperiments/src/Main.java
+++ 4.1IntegerExperiments/IntegerExperiments/src/Main.java
@@ -2,6 +2,9 @@
     TODO:
      1. Запустите код проекта IntegerExperiments. Найдите ошибку, исправьте её заменой Integer на int.
      2. Реализуйте статический метод sumDigits, чтобы он считал сумму цифр в числе и возвращал сумму в результате своей работы.
+     3.	Изучите методы класса Character, найдите метод получения int из char.
+     4.	Напишите метод sumDigitsChar() в котором используйте метод класса Character.
+
 */
 
 public class Main
@@ -11,6 +14,7 @@ public class Main
         Container container = new Container();
         container.count += 7843;
         System.out.println(sumDigits(container.count));
+        System.out.println(sumDigitsChar(container.count));
 
     }
 
@@ -18,8 +22,18 @@ public class Main
     {
         int sum = 0;
         String str = number.toString();
-        for (int idx = 0; str.length() > idx; idx++){
+        for (int idx = 0; idx < str.length() ; idx++){
             sum += Integer.parseInt(String.valueOf(str.charAt(idx)));
+        }
+
+        return sum;
+    }
+
+    public static Integer sumDigitsChar(Integer number)
+    {
+        int sum = 0;
+        for (int idx=0; idx < number.toString().length() ;idx++){
+            sum += Character.getNumericValue(number.toString().charAt(idx));
         }
 
         return sum;
