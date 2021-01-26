2b3ee67ffc337401036ed36a1a499f864f4c00ab
Akvamarinika <rozespink@mail.ru>
2021-01-26 19:46:38 +0800

    Fix bugs Add phone && Add Name

--- 5.4 Map/src/PhoneBook.java
+++ 5.4 Map/src/PhoneBook.java
@@ -8,35 +8,38 @@ import java.util.*;
    2. Определяйте имя и телефон с помощью регулярных выражений.
 */
 public class PhoneBook {
-    private static Map<String, Set<String> > name2phone = new HashMap<>();
+    private static Map<String, Set<String> > name2phone = new TreeMap<>();
     private static Scanner scanner = new Scanner(System.in);
     private static final String PHONE_REGIX = "^(\\+7|7|8)\\s*[-(]*\\d{3}[-)]*\\s*\\d{3}[-\\s]*\\d{2}[-\\s]*\\d{2}$";
     private static final String NAME_REGIX = "[a-zA-z]+\\s?[a-zA-z]*";
     public static void main(String[] args){
         fillWithTestData();
-        String input = scanner.nextLine();
-        if (input.equals("LIST")){
-            printPhoneBook();
-        }else if(input.matches(NAME_REGIX)){
-            addNewName(input);
-        }else if(input.matches(PHONE_REGIX)){
-            addNewPhone(input);
+        while (true) {
+            System.out.println("\nДля вывода всех контактов используйте команду LIST. \nВведите тел./имя контакта: ");
+            String input = scanner.nextLine();
+            if (input.equals("LIST")) {
+                printPhoneBook();
+            } else if (input.matches(NAME_REGIX)) {
+                addNewName(input);
+            } else if (input.matches(PHONE_REGIX)) {
+                addNewPhone(input);
+            }
         }
-
-
     }
 
-    private static void addNewName(String inputUser){
-        StringBuilder builder = new StringBuilder(inputUser.toLowerCase());
-        String symbol = Character.toString(Character.toUpperCase(inputUser.charAt(0)));
-        builder.replace(0,1, symbol);
-        if (name2phone.containsKey(inputUser)){
-            printContact(inputUser);
+    private static void addNewName(String inputName){
+        inputName = nameFormat(inputName);
+        String inputPhone;
+        if (name2phone.containsKey(inputName)){
+            printContact(inputName);
         }else {
-            System.out.println("Input tel. contact " + inputUser + ": ");
-
-            if (scanner.nextLine().matches(PHONE_REGIX)){
-
+            System.out.println("Введите тел. для контакта " + inputName + ": ");
+            inputPhone = scanner.nextLine();
+            if (inputPhone.matches(PHONE_REGIX)){
+                inputPhone = numberFormat(inputPhone);
+                addNewSet(inputName,inputPhone);
+            }else {
+                System.out.println("Ошибка: Неверно введен тел.!");
             }
 
         }
@@ -44,31 +47,47 @@ public class PhoneBook {
 
     private static void addNewPhone(String inputUser){
         boolean flag = false;
+        String formatPhone = numberFormat(inputUser);
+        for (String key : name2phone.keySet()){
+            if(name2phone.get(key).contains(formatPhone)){ // Set name2phone.get(key).
+                printContact(key);
+                flag = true;
+                break;
+            }
+        }
+        if (!flag){
+            System.out.println("Введите имя контакта для тел. " + formatPhone +": ");
+            String inputName = scanner.nextLine();
+            if (inputName.matches(NAME_REGIX)){
+                inputName = nameFormat(inputName);
+                addNewSet(inputName,formatPhone);
+            }else {
+                System.out.println("Ошибка: Неверно введено имя контакта!");
+            }
+        }
+
+    }
+
+    private static String nameFormat(String inputUser) {
+        StringBuilder builder = new StringBuilder(inputUser.trim().toLowerCase());
+        String symbol = Character.toString(Character.toUpperCase(inputUser.charAt(0)));
+        builder.replace(0, 1, symbol);
+        String symbolSurname = Character.toString(Character.toUpperCase(builder.charAt(builder.indexOf(" ") + 1)));
+        builder.replace(builder.indexOf(" ") + 1,builder.indexOf(" ") + 2, symbolSurname);
+        return builder.toString();
+    }
+
+    private static String numberFormat(String inputUser){
         String phone = inputUser.replaceAll("[\\s-()]*","");
-        phone = phone.replaceAll("(\\+7|7)*","8");
+        phone = phone.replaceAll("^(\\+7|7)","8");
         StringBuilder builderPhone = new StringBuilder(phone);
         builderPhone.insert(1,"(");
         builderPhone.insert(5,")");
         builderPhone.insert(9,"-");
         builderPhone.insert(12,"-");
-        for (Map.Entry entry : name2phone.entrySet()){
-            Set set = entry.getValue();
-            for (Set arr : entry.getValue()){
-                if (arr.contains(builderPhone.toString())){
-                    flag = true;
-                    break;
-                }
-            }
-        }
-
-        if (flag){
-            printContact(inputUser);////
-        }
-        name2phone.containsValue();
-        if (builderPhone.toString()){
-
-        }
+        return builderPhone.toString();
     }
+
     private static void printContact(String key){
         System.out.printf("\nContact %s: \ntel.: %s", key, name2phone.get(key));
     }
@@ -78,7 +97,7 @@ public class PhoneBook {
         }
     }
 
-    private static void checkSet(String key, String value){
+    private static void addNewSet(String key, String value){
         if (!name2phone.containsKey(key)){
             name2phone.put(key, new HashSet<String>());
         }
@@ -86,12 +105,12 @@ public class PhoneBook {
     }
 
     private static void fillWithTestData(){
-        checkSet("Amy", "8(950)128-45-84");
-        checkSet("Victor", "8(914)333-11-11");
-        checkSet("Dave", "8(924)000-12-34");
-        checkSet("Pavel", "8(914)914-00-00");
-        checkSet("Alex", "8(950)111-22-22");
-        checkSet("Victor", "8(914)333-11-11");
+        addNewSet("Amy", "8(950)128-45-84");
+        addNewSet("Victor", "8(914)333-11-11");
+        addNewSet("Dave", "8(924)000-12-34");
+        addNewSet("Pavel", "8(914)914-00-00");
+        addNewSet("Alex", "8(950)111-22-22");
+        addNewSet("Victor", "8(914)333-11-11");
        // name2phone.put("Alex", "8(950)111-22-22");
     }
 
