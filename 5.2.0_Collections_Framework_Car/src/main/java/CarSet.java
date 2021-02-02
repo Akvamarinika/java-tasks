c9208eee8fa5f908fe6c64714a25310d61690321
Akvamarinika <rozespink@mail.ru>
2021-02-02 23:54:57 +0800

    Added init Collections Set

---
+++ 5.2.0_Collections_Framework_Car/src/main/java/CarSet.java
@@ -0,0 +1,21 @@
+public class CarSet implements CarSetMethods {
+    @Override
+    public boolean add(Car car) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Car car) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}
