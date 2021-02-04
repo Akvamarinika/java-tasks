cd9eae5ce5d98f745df6910f0861b688402ccc84
Akvamarinika <rozespink@mail.ru>
2021-02-05 03:14:45 +0800

    Added test foreach()

---
+++ 5.2.0_Collections_Framework_Car/src/test/java/CollectionMethodsTest.java
@@ -0,0 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CollectionMethodsTest {
+    private CollectionMethods carList;
+
+    @Before
+    public void setUp() throws Exception {
+        carList = new CarHashSet();
+        for (int i = 0; i < 100; i++){
+            carList.add(new Car());
+        }
+    }
+
+    @Test
+    public void testForeach(){
+        int index = 0;
+        for (Car car : carList){
+            index++;
+        }
+        assertEquals(100, index);
+    }
+}
