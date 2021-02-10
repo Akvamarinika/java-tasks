b754c90724303d72289015e784e2f60ec2bb2238
Akvamarinika <rozespink@mail.ru>
2021-02-11 00:58:18 +0800

    Refactoring & fix bug in method transfer types

--- 8.0.3GenericsSubtyping/src/main/java/Basket.java
+++ 8.0.3GenericsSubtyping/src/main/java/Basket.java
@@ -4,8 +4,8 @@ import java.util.List;
 public class Basket<T extends Fruit>{
     private List<T> basket = new ArrayList<>();
 
-    public double getWeight(){
-        double total = 0;
+    public float getWeight(){
+        float total = 0f;
         for (T fruit : basket){
             total += fruit.weight;
         }
@@ -17,16 +17,10 @@ public class Basket<T extends Fruit>{
     }
 
     public int compare(Basket<? extends Fruit> other){
-        if (getWeight() > other.getWeight()){
-            return 1;
-        } else if (getWeight() < other.getWeight()){
-            return -1;
-        }else {
-            return 0;
-        }
+        return Float.compare(getWeight(), other.getWeight());
     }
 
-    public static <E extends Fruit> void transfer(Basket<? extends E> src, Basket<E> dct){
+    public static <E extends Fruit> void transfer(Basket<? extends E> src, Basket<? super E> dct){
         dct.basket.addAll(src.basket);
         src.basket.clear();
     }
