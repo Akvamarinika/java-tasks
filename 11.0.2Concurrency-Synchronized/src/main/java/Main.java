9b658a93ca5ad46ac0722bafeba6be14051ca505
Akvamarinika <rozespink@mail.ru>
2021-02-15 22:50:36 +0800

    With synchronized()

---
+++ 11.0.2Concurrency-Synchronized/src/main/java/Main.java
@@ -0,0 +1,65 @@
+public class Main {
+    public static void main(String[] args) {
+        Atm atm = new Atm();
+
+        Thread client1 = new Thread(new Runnable(){
+            private String name = "Ivan";
+            public String getName() {
+                return name;
+            }
+
+            @Override
+            public void run() {
+                synchronized (atm){
+                    atm.withdrawMoney(this.getName(), 50000);
+                }
+
+            }
+        });
+
+        Thread client2 = new Thread(new Runnable() {
+            private String name = "Kate";
+            public String getName() {
+                return name;
+            }
+            @Override
+            public void run() {
+                /*try {
+                    client1.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }*/
+                synchronized (atm){
+                  atm.withdrawMoney(this.getName(), 80000);
+                }
+
+            }
+        });
+
+        Thread client3 = new Thread(new Runnable() {
+            private String name = "Alex";
+            public String getName() {
+                return name;
+            }
+            @Override
+            public void run() {
+                /*try {
+                    client1.join();
+                    client2.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }*/
+                synchronized (atm){
+                    atm.withdrawMoney(this.getName(), 50000);
+                }
+
+
+            }
+        });
+
+        client1.start();
+        client2.start();
+        client3.start();
+
+    }
+}
