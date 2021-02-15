3a0cdabd79fbb3b80b8a70e9de67fc88868df2e5
Akvamarinika <rozespink@mail.ru>
2021-02-15 21:43:33 +0800

    init

---
+++ 11.0.2Concurrency-Synchronized/src/main/java/Atm.java
@@ -0,0 +1,22 @@
+public class Atm {
+
+    private int balance = 100_000;
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void withdrawMoney(String name, int amount){
+        System.out.println(name + " подошел к банкомату");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if (balance >= amount){
+            System.out.println(name + " вывел " + amount + " руб.");
+        } else {
+            System.out.println("В банкомате недостаточно денег для " + name);
+        }
+    }
+}
