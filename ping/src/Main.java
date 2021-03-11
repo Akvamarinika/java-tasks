a9de79ac961c2235a1b2812e5f601cbc81636866
Akvamarinika <rozespink@mail.ru>
2021-03-12 01:41:52 +0800

    Added counter

--- ping/src/Main.java
+++ ping/src/Main.java
@@ -1,45 +1,41 @@
 import java.io.*;
 import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
+    private static Map<String, Integer> timeCount = new HashMap<>();
     public static void main(String[] args) {
         //writeFilePing();
         readAndWriteTimeSendPackage();
+        System.out.println(timeCount);
 
     }
 
-   /* public static void ping(){
-        Process ping = null;
-        try {
-            ping = Runtime.getRuntime().exec("ping -n 1000 google.com");
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void counter(String timeKey){
+        if (timeCount.containsKey(timeKey)){
+            int value = timeCount.get(timeKey) + 1;
+            timeCount.put(timeKey, value);
+        }else {
+            timeCount.put(timeKey, 1);
         }
-
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(ping.getInputStream(), Charset.forName("CP866")))){
-            String line;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-            }
-        }catch(IOException e){
-            e.printStackTrace();
-        }
-
-    }*/
+    }
 
     public static void readAndWriteTimeSendPackage(){
         try(BufferedReader br = new BufferedReader(new FileReader("file.txt"));
-            BufferedWriter bw = new BufferedWriter(new FileWriter("filter.txt", true));
+            BufferedWriter bw = new BufferedWriter(new FileWriter("filter.txt"))
         ){
             String line;
             while ((line = br.readLine()) != null) {
-                int strLen1 = "время=".length()-1;
-                int strLen2 = "время=".length()-1;
-                int idxStart = line.indexOf("время=") + strLen1;
+                int strLen = "время=".length();
+                int idxStart = line.indexOf("время=") + strLen;
                 int idxEnd = line.indexOf("мс TTL");
-                String time = line.substring(idxStart, idxEnd);
-                bw.write(time + '\n');
-                bw.flush();
+                if (idxStart != -1 && idxEnd != -1) {
+                    String time = line.substring(idxStart, idxEnd);
+                    counter(time);
+                    bw.write(time + '\n');
+                    bw.flush();
+                }
             }
         }catch(IOException e){
             e.printStackTrace();
