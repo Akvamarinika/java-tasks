2c2765825a846b1a222875f6b0d2b2e4a2bb68d5
Akvamarinika <rozespink@mail.ru>
2021-03-19 23:51:47 +0800

    Added task 11.1 ThreadsImageResizer

---
+++ 11.1ThreadsImageResizer/src/Main.java
@@ -0,0 +1,41 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        String srcFolder = "H:\\src";
+        String dstFolder = "H:\\dst";
+
+        File srcDir = new File(srcFolder);
+        long start = System.currentTimeMillis();
+        File[] files = srcDir.listFiles();
+        
+        if (files != null) {
+            int lenForThread = files.length / 4;
+
+            File[] files1 = new File[lenForThread];
+            System.arraycopy(files, 0, files1, 0, lenForThread);
+            File[] files2 = new File[lenForThread];
+            System.arraycopy(files, lenForThread, files2, 0, lenForThread);
+            File[] files3 = new File[lenForThread];
+            System.arraycopy(files, lenForThread * 2, files3, 0, lenForThread);
+            File[] files4 = new File[files.length - (lenForThread * 3)];
+            System.arraycopy(files, lenForThread * 3, files4, 0, files.length - (lenForThread * 3));
+
+            ResizerImg resizerImg1 = new ResizerImg(files1, dstFolder, start);
+            new Thread(resizerImg1).start();
+
+            ResizerImg resizerImg2 = new ResizerImg(files2, dstFolder, start);
+            new Thread(resizerImg2).start();
+
+            ResizerImg resizerImg3 = new ResizerImg(files3, dstFolder, start);
+            new Thread(resizerImg3).start();
+
+            ResizerImg resizerImg4 = new ResizerImg(files4, dstFolder, start);
+            new Thread(resizerImg4).start();
+
+
+        }
+    }
+}
