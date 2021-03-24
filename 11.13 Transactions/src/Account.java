120ce8d1e5c4f500518789ce6cc8ae8a6895010b
Akvamarinika <rozespink@mail.ru>
2021-03-24 23:30:44 +0800

    Added getBalance

--- 11.13 Transactions/src/Account.java
+++ 11.13 Transactions/src/Account.java
@@ -1,5 +1,64 @@
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Account
 {
-    private long money;
+    private AtomicLong money = new AtomicLong(0);
     private String accNumber;
+    private boolean isBlocked;
+
+    public Account(long money, String accNumber){
+        this.money.set(money);
+        this.accNumber = accNumber;
+        isBlocked = false;
+    }
+
+    public boolean getIsBlocked(){
+        return isBlocked;
+    }
+
+    public String getAccNumber(){
+        return accNumber;
+    }
+
+    public long getMoney(){
+        return money.get();
+    }
+
+    public boolean replenish(long amount) throws Exception {
+        if (amount > 0 && !getIsBlocked()) {
+            this.money.accumulateAndGet(amount, (balance, s) -> balance + s);
+            return true;
+        } else {
+            throw new Exception("Sorry, account is blocked or amount < 0 ");
+        }
+    }
+
+    public boolean withdraw(long amount) throws Exception{
+        if (amount < money.get() && amount > 0 && !getIsBlocked()){
+            this.money.accumulateAndGet(amount, (balance, s) -> balance - s);
+            return true;
+        }else{
+            throw  new Exception("Sorry, account is blocked or amount < 0 or amount > balance");
+        }
+
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Account){
+            Account account = (Account) obj;
+            if (this.accNumber.equals(account.accNumber)){
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accNumber);
+    }
 }
--- 11.13 Transactions/src/Bank.java
+++ 11.13 Transactions/src/Bank.java
@@ -5,8 +5,9 @@ public class Bank
 {
     private HashMap<String, Account> accounts;
     private final Random random = new Random();
+    private static final int MAX_AMOUNT = 50_000;
 
-    public synchronized boolean isFraud(String fromAccountNum, String toAccountNum, long amount)
+    public synchronized boolean isFraud(String fromAccountID, String toAccountID, long amount)
         throws InterruptedException
     {
         Thread.sleep(1000);
@@ -20,16 +21,57 @@ public class Bank
      * метод isFraud. Если возвращается true, то делается блокировка
      * счетов (как – на ваше усмотрение)
      */
-    public void transfer(String fromAccountNum, String toAccountNum, long amount)
+    public void transfer(String fromAccountID, String toAccountID, long amount, boolean isBlocked) throws Exception
     {
+        if(getAccount(fromAccountID).getIsBlocked() || getAccount(toAccountID).getIsBlocked()){
+            throw new Exception("Sorry, one from Account is Blocked!");
+        }
 
+        if(getAccount(fromAccountID).equals(getAccount(fromAccountID))){
+            throw new Exception("Sorry, two identical bank accounts!");
+        }
+
+        if (amount > MAX_AMOUNT){
+            try {
+                boolean isFraud = isFraud(fromAccountID, toAccountID, amount);
+                if (isFraud){
+
+                }
+            } catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Sorry, you account is blocked!");
+        }
     }
 
     /**
      * TODO: реализовать метод. Возвращает остаток на счёте.
      */
-    public long getBalance(String accountNum)
-    {
-        return 0;
+    public long getBalance(String accountID){
+        return getAccount(accountID).getMoney();
+    }
+
+    public boolean isExistAccountID(String accountID) {
+            return accounts.containsKey(accountID);
+    }
+
+    public Account getAccount(String accountID){
+        if(isExistAccountID(accountID)){
+            return accounts.get(accountID);
+        } else {
+
+            try {
+                throw new Exception(accountID);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+        return null;
+    }
+
+    public void blockedAccount(String accountID){
+        //getAccount(accountID)
     }
 }
