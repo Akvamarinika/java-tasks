f49fb9cbcfe4b27ad570feb219dbef515cee4937
Akvamarinika <rozespink@mail.ru>
2021-03-28 02:44:02 +0800

    Add method APIRequest

---
+++ azure-speech/src/main/java/Speech.java
@@ -0,0 +1,104 @@
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+import com.microsoft.cognitiveservices.speech.AudioDataStream;
+import com.microsoft.cognitiveservices.speech.SpeechConfig;
+import com.microsoft.cognitiveservices.speech.SpeechSynthesizer;
+import com.microsoft.cognitiveservices.speech.SpeechSynthesisOutputFormat;
+import com.microsoft.cognitiveservices.speech.SpeechSynthesisResult;
+import com.microsoft.cognitiveservices.speech.audio.AudioConfig;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Speech {
+    private static final String KEY = "be5f4c47488e4d349dbb06b527492c7c";
+    private static final String REGION = "francecentral";
+    private static final String AUTH_URL = "https://francecentral.api.cognitive.microsoft.com/sts/v1.0/issueToken";
+    private static HttpURLConnection httpURLConnection = null;
+
+    public static void main(String[] args) {
+        try {
+            String nameFile = getFileName();
+            String myText = readText(nameFile);
+            String token = getToken();
+            System.out.println(token);
+            APIRequest(myText);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getFileName() {
+        System.out.println("Enter name source file name: ");
+        String fileName = new Scanner(System.in).nextLine().trim();
+        return fileName;
+    }
+
+    public static String readText(String fileName) throws IOException {
+        String line;
+        StringBuilder builder = new StringBuilder();
+        try(BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            while ((line = reader.readLine()) != null){
+                builder.append(line);
+                builder.append('\n');
+            }
+        }
+        return builder.toString();
+    }
+
+    public static void APIRequest(String yourText) {
+        String gender = "MALE";
+        String voice = "bg-BG-IvanRUS";
+        String requestBody = "<speak version='1.0' xml:lang='en-US'><voice xml:lang='en-US' xml:gender='" + gender + "' name='" + voice + "'>" + yourText + "</voice></speak>";
+
+        SpeechConfig speechConfig = SpeechConfig.fromSubscription(KEY, REGION);
+        AudioConfig audioConfig = AudioConfig.fromWavFileOutput("path/to/write/file.wav");
+
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer(speechConfig, null);
+
+        SpeechSynthesisResult result = synthesizer.SpeakSsml(requestBody);
+        AudioDataStream stream = AudioDataStream.fromResult(result);
+        stream.saveToWavFile("voice.wav");
+    }
+
+    public static void averageCharDurationInSec(){
+
+    }
+
+    public static String getToken() throws IOException {
+        sendRequest();
+        return getResponse();
+    }
+
+    public static void sendRequest() throws IOException {
+        String POSTText = "";
+        URL url = new URL(AUTH_URL);
+        httpURLConnection = (HttpURLConnection) url.openConnection();
+        httpURLConnection.setDoOutput(true); // set POST
+        httpURLConnection.setRequestProperty("Ocp-Apim-Subscription-Key", KEY);
+        httpURLConnection.setRequestProperty("Content-Length", "0");
+        httpURLConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+        try (OutputStream output = httpURLConnection.getOutputStream()) {
+            output.write(POSTText.getBytes());
+        }
+    }
+
+    public static String getResponse() throws IOException {
+        InputStream input = httpURLConnection.getInputStream();
+        BufferedInputStream bufferIn = new BufferedInputStream(input);
+        StringBuilder builder = new StringBuilder();
+        byte[] array = new byte[512];
+        int count = bufferIn.read(array);
+        while (count > 0){
+            builder.append(new String(array, 0, count));
+            count = bufferIn.read(array);
+        }
+        //System.out.println(builder.toString());
+        httpURLConnection.disconnect();
+        return builder.toString();
+    }
+}
