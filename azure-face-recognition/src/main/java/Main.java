e6200be8fdb0955a7a5585e5101e8ef8a4a730f8
Akvamarinika <rozespink@mail.ru>
2021-05-08 03:59:19 +0800

    Added parseJSON & croppingAnImage

--- azure-face-recognition/src/main/java/Main.java
+++ azure-face-recognition/src/main/java/Main.java
@@ -1,14 +1,21 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.io.*;
 import java.util.List;
 import java.util.Map;
-
+//https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Collage_of_Six_Cats-01.jpg/1200px-Collage_of_Six_Cats-01.jpg
 public class Main {
     private static final String URL_FOR_APIRequest = "https://westeurope.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false&recognitionModel=recognition_04&returnRecognitionModel=false&detectionModel=detection_03&faceIdTimeToLive=86400";
     private static HttpURLConnection httpURLConnection = null;
     private static final String KEY = "01f0026ec4bc4880b6b8713dbd581e97";
     private static final String JSON_CONTENT = "{\"url\": \"https://4.bp.blogspot.com/-yAIaurHWbDs/UjcXMiM4QMI/AAAAAAAAWDI/sklohMF7XRA/w1200-h630-p-k-no-nu/changingface+ofyoda.jpg\"}";
+    private static List<Face> faceList;
 
     public static void main(String[] args) {
         try {
@@ -16,11 +23,42 @@ public class Main {
             uploadJSON();
             String json = getResponse();
             System.out.println(json);
+            parseJSON(json);
+            System.out.println(faceList);
+            croppingAnImage();
+           /* int y = Integer.parseInt(faceList.get(0).getFaceRectangle().getTop());
+            int x = Integer.parseInt(faceList.get(0).getFaceRectangle().getLeft());
+            int width = Integer.parseInt(faceList.get(0).getFaceRectangle().getWidth());
+            int height = Integer.parseInt(faceList.get(0).getFaceRectangle().getHeight());
 
+            BufferedImage img= ImageIO.read(new File("src\\main\\resources\\changingface ofyoda.jpg"));
+            BufferedImage tile = img.getSubimage(x, y, width, height);
+            ImageIO.write(tile, "jpg", new File("Square.jpg"));
+            System.out.println(tile);*/
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
+
+    public static void croppingAnImage() throws IOException {
+        File file = new File("src\\main\\resources\\changingface ofyoda.jpg");
+        BufferedImage img= ImageIO.read(file);
+        BufferedImage dstImg;
+        int i = 1;
+        int y;
+        int x;
+        int width;
+        int height;
+        for (Face face : faceList){
+           y =  Integer.parseInt(face.getFaceRectangle().getTop());
+           x = Integer.parseInt(face.getFaceRectangle().getLeft());
+           width = Integer.parseInt(face.getFaceRectangle().getWidth());
+           height = Integer.parseInt(face.getFaceRectangle().getHeight());
+           dstImg = img.getSubimage(x, y, width, height);
+           ImageIO.write(dstImg, "jpg", new File(file.getName().substring(0, file.getName().length()-4) + i + ".jpg"));
+           i++;
+        }
     }
 
     public static void APIRequest() throws IOException {
@@ -37,9 +75,8 @@ public class Main {
     }
 
     private static void uploadJSON() throws IOException {
-        String jsonContent = "{\"url\": \"https://4.bp.blogspot.com/-yAIaurHWbDs/UjcXMiM4QMI/AAAAAAAAWDI/sklohMF7XRA/w1200-h630-p-k-no-nu/changingface+ofyoda.jpg\"}";
         try ( OutputStreamWriter  writer = new OutputStreamWriter(httpURLConnection.getOutputStream(), "UTF-8");) {
-            writer.write(jsonContent);
+            writer.write(JSON_CONTENT);
             writer.close();
 
             System.out.println(httpURLConnection.getResponseMessage());
@@ -61,4 +98,12 @@ public class Main {
         }
         return builder.toString();
     }
+
+    public static void parseJSON(String jsonArray) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        //mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+         faceList = mapper.readValue(jsonArray, new TypeReference<List<Face>>() {});
+
+    }
+
 }
